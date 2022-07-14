e15585f9be05f0708512db192536defa367071b5
Lucas Viana <oviana@outlook.com>
2022-07-14 01:07:07 -0300

    Func: add data field validation + improve north/south detection logic

--- src/functionalities.ts
+++ src/functionalities.ts
@@ -1,21 +1,24 @@
 import { CountryHemispheres, CovidData } from './model';
-import { getNHighest, NORTHERN_HEMISPHERE, SOUTHERN_HEMISPHERE } from './utils';
+import { filterInvalidAttributes, getNHighest } from './utils';
 
 /** Find the names (sorted) of the three coutries with most confirmed cases in a dataset. */
-export const threeCountriesMostConfirmed = (data: CovidData[]): string[] =>
-  getNHighest<CovidData>(data, 3, (a, b) => a.confirmed - b.confirmed)
+export const threeCountriesMostConfirmed = (data: CovidData[]): string[] => {
+  data = filterInvalidAttributes({ confirmed: 'string' }, data);
+  return getNHighest<CovidData>(data, 3, (a, b) => a.confirmed - b.confirmed)
     .map(element => element.country)
     .sort();
+};
 
-/** Find the countries with mots death for the northern and southern hemispheres. */
+/** Find the countries with most deaths for the each hemisphere. */
 export const countryMostDeathsByHemisphere = (data: CovidData[]): CountryHemispheres => {
-  let northern: CovidData = { country: '', active: 0, deaths: 0, confirmed: 0 };
-  let southern: CovidData = { country: '', active: 0, deaths: 0, confirmed: 0 };
+  data = filterInvalidAttributes({ latitude: 'number', deaths: 'number' }, data);
+  let northern: CovidData = { country: '', active: 0, deaths: 0, confirmed: 0, latitude: 0 };
+  let southern: CovidData = { country: '', active: 0, deaths: 0, confirmed: 0, latitude: 0 };
 
   data.forEach(row => {
-    if (NORTHERN_HEMISPHERE.has(row.country) && row.deaths > northern.deaths) {
+    if (row.latitude >= 0 && row.deaths > northern.deaths) {
       northern = row;
-    } else if (SOUTHERN_HEMISPHERE.has(row.country) && row.deaths > southern.deaths) {
+    } else if (row.latitude < 0 && row.deaths > southern.deaths) {
       southern = row;
     }
   });
@@ -24,13 +27,17 @@ export const countryMostDeathsByHemisphere = (data: CovidData[]): CountryHemisph
 };
 
 /** Sum "active" field of all countries with at least 1,000,000 confirmed cases. */
-export const sumActiveForManyCases = (data: CovidData[]): number =>
-  data.reduce((acc, cur) => acc + (cur.confirmed >= 1e6 ? cur.active : 0), 0);
+export const sumActiveForManyCases = (data: CovidData[]): number => {
+  data = filterInvalidAttributes({ active: 'number', confirmed: 'number' }, data);
+  return data.reduce((acc, cur) => acc + (cur.confirmed >= 1e6 ? cur.active : 0), 0);
+}
 
 /** Sum "death" fields for the 5 countries with the fewest active cases for the 10 with the most confirmed cases. */
-export const sumFewestDeathsInMostActives = (data: CovidData[]): number =>
-  getNHighest<CovidData>(
+export const sumFewestDeathsInMostActives = (data: CovidData[]): number => {
+  data = filterInvalidAttributes({ active: 'number', confirmed: 'number', deaths: 'number' }, data);
+  return getNHighest<CovidData>(
     getNHighest<CovidData>(data, 10, (a, b) => a.active - b.active),
     5,
     (a, b) => b.confirmed - a.confirmed
   ).reduce((acc, cur) => acc + cur.deaths, 0);
+};
